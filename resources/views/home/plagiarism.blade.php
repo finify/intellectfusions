386cb04168025dd1894caf2c50b2b72c66f6f0ff
finify <ifeanyiizuegbu@yahoo.com>
2024-06-13 21:27:31 +0100

    fsfds

---
+++ resources/views/home/plagiarism.blade.php
@@ -0,0 +1,35 @@
+@extends('home.layout.layout')
+
+@section('header')
+<link rel="stylesheet" href="/homeassets/css/course.min.css" />
+<link rel="stylesheet" href="/homeassets/css/about.min.css" />
+<link rel="stylesheet" href="/homeassets/css/index.min.css" />
+@endsection
+@section('content')
+<header class="page">
+    <div class="page_breadcrumbs">
+        <div class="container">
+            <ul class="page_breadcrumbs-list d-flex flex-wrap align-items-center">
+                <li class="list-item">
+                    <a href="/" class="link">Home</a>
+                </li>
+
+                <li class="list-item">Plagiarism</li>
+            </ul>
+        </div>
+    </div>
+    <div class="page_main">
+        <div class="underlay"></div>
+        <div class="container">
+            <div class="content-wrapper">
+                <h1 class="page_main-header">Coming Soon</h1>
+                <p class="page_main-text">
+               
+                </p>
+
+            </div>
+        </div>
+    </div>
+</header>
+
+@endsection
