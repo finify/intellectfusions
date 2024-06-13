86a0f6ab0300123091163b3925cef237ef4cc5c2
finify <ifeanyiizuegbu@yahoo.com>
2024-06-13 18:05:58 +0100

    sdfadfds

--- app/Http/Controllers/Admin/ProjectsController.php
+++ app/Http/Controllers/Admin/ProjectsController.php
@@ -233,7 +233,7 @@ class ProjectsController extends Controller
                 $project_detail = [
                     'progress'=> $data['projectstatus']
                 ];
-                dd($expertid);
+                dd($data['expertid']);
                 
                 $projectupdated = projects::where('id',$slug)->update($project_detail);
 
