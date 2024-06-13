49044196bea1bf548eaf3ee81ebece01ec7f8262
finify <ifeanyiizuegbu@yahoo.com>
2024-06-13 16:30:13 +0100

    this

--- app/Http/Controllers/Expert/ExpertController.php
+++ app/Http/Controllers/Expert/ExpertController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 use App\Models\User;
 use App\Models\projects;
