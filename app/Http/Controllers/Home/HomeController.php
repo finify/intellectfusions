3026fa26674d7986ceea40f8225b346664ececae
finify <ifeanyiizuegbu@yahoo.com>
2024-06-13 21:28:22 +0100

    fdsfs

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -48,6 +48,7 @@ class HomeController extends Controller
     }
 
     public function plagiarism(){
-        return view('home.plagiarism');
+        $details = $this->getDetails();
+        return view('home.plagiarism')->with($details);
     }
 }
