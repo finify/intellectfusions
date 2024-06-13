ea0491e357cdac47bce45e7f871b3f0598c02b8d
finify <ifeanyiizuegbu@yahoo.com>
2024-06-13 17:00:14 +0100

    hh

--- app/Http/Controllers/Expert/ExpertController.php
+++ app/Http/Controllers/Expert/ExpertController.php
@@ -41,6 +41,7 @@ class ExpertController extends Controller
         $auctions = DB::table('project_expert')
         ->join('projects', 'project_expert.project_id', '=', 'projects.id')
         ->where('project_expert.expert_id', Auth::guard('expert')->User()->id)
+        ->where('project_expert.status', 0 )
         ->select('projects.*', 'project_expert.*')->orderBy('project_expert.id','desc','expert','projects')
         ->get();
         //withdrawal details
